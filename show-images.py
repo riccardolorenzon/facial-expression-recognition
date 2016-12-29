f5c1e715e761b5566355888c1596bcaac44c81b0
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2016-12-29 15:40:45 +0100

    fix for Python 3.x

--- show-images.py
+++ show-images.py
@@ -1,21 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from util import getData
+from utils import getData
 
 label_map = ['Anger', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 def main():
-    X, Y = getData(balace_ones=False)
+    X, Y = getData(balance_ones=False)
 
     while True:
-        for i in xrange(7):
+        for i in range(7):
             x, y = X[Y==i], Y[Y==i]
             N = len(y)
             j = np.random.choice(N)
             plt.imshow(x[j].reshape(48,48), cmap='gray')
             plt.title(label_map[y[j]])
-        prompt = raw_input('Quit? Enter Y:\n')
+        prompt = input('Quit? Enter Y:\n')
         if prompt == 'Y':
             break
 
